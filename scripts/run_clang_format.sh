ea809c4b22ab5e94b576829218796279a262f4f4
f-squirrel <dmitry.b.danilov@gmail.com>
2020-06-21 15:13:39 +0300

    Add targets make format and make format-check

--- scripts/run_clang_format.sh
+++ scripts/run_clang_format.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
-find . -path ./build -prune -o \( -name '*.hpp' -or -name '*.cpp' \) \
-    -exec clang-format -verbose -style=file -fallback-style=none -i {} \;
+if [[ -n $2 ]]; then
+    find $1 -path ${1}/build -prune -o \( -name '*.hpp' -or -name '*.cpp' \) \
+        -exec clang-format -verbose -style=file -fallback-style=none $2 {} \;
+else
+    NUMBER_OF_CHANGES=$(find $1 -path ${1}/build -prune -o \( -name '*.hpp' -or -name '*.cpp' \) \
+        -exec clang-format -verbose -style=file -fallback-style=none -output-replacements-xml {} \; \
+      | grep "<replacement offset" \
+      | wc -l)
+    if [ ${NUMBER_OF_CHANGES} -ne 0 ]; then
+      echo "Code format changes needed"
+      exit 1
+    else
+      echo "No format changes needed"
+      exit 0
+    fi
+fi
