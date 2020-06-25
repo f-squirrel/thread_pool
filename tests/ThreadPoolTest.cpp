788c4fccce6227b8126106c87b3557e321b1c9d6
f-squirrel <dmitry.b.danilov@gmail.com>
2020-06-25 18:29:03 +0300

    Improve tests

--- tests/ThreadPoolTest.cpp
+++ tests/ThreadPoolTest.cpp
@@ -1,142 +1,66 @@
-#include <gtest/gtest.h>
-
-#include <stdexcept>
-#include <thread_pool/ThreadPool.hpp>
-
 #include <chrono>
-#include <iostream>
+#include <future>
 #include <mutex>
 #include <set>
+#include <stdexcept>
 #include <thread>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include <thread_pool/ThreadPool.hpp>
 
 using namespace thread_pool;
 using namespace std::chrono_literals;
 
-struct Task {
-    Task(std::set<std::thread::id>& s, std::mutex& m) : _ids(s), _mutex(m) {}
-    void operator()() {
-        {
-            std::unique_lock<std::mutex> lock{_mutex};
-            _ids.insert(std::this_thread::get_id());
-        }
-        std::this_thread::sleep_for(1s);
-    }
-    std::set<std::thread::id>& _ids;
-    std::mutex& _mutex;
-};
-
 TEST(ThreadPool, RunMoreTasksThanThreads) {
-
-    std::mutex mutex;
-    std::set<std::thread::id> set;
     const size_t THREAD_COUNT{2u};
     const size_t TASK_COUNT{20u};
-    std::vector<std::future<typename std::result_of<Task()>::type>> v;
-    {
-        ThreadPool pool{THREAD_COUNT};
-        for (size_t i = 0; i < TASK_COUNT; ++i) {
-            v.push_back(pool.submit(Task(set, mutex)));
-        }
 
-        std::this_thread::sleep_for(1s);
-        EXPECT_TRUE(v.size() == TASK_COUNT);
-        for (size_t i = 0; i < TASK_COUNT; ++i) {
-            v[i].get();
-        }
-        EXPECT_EQ(pool.queueSize(), 0u);
-    }
-}
-
-struct IntegerTask : public Task {
-    size_t _task_num;
-    IntegerTask(std::set<std::thread::id>& s, std::mutex& m, size_t task_num)
-        : Task(s, m), _task_num{task_num} {}
-    size_t operator()() {
-        {
-            std::unique_lock<std::mutex> lock{_mutex};
-            _ids.insert(std::this_thread::get_id());
-        }
-        std::this_thread::sleep_for(1s);
-        return _task_num;
-    }
-};
-
-TEST(ThreadPool, ReturnIntegers) {
     std::mutex mutex;
-    std::set<std::thread::id> set;
-    const size_t TASK_COUNT{4u};
-    std::vector<std::future<typename std::result_of<IntegerTask()>::type>> v;
-    {
-        ThreadPool pool{2u};
-        for (size_t i = 0; i < TASK_COUNT; ++i) {
-            v.push_back(pool.submit(IntegerTask(set, mutex, i)));
-        }
+    size_t result = 0u;
+    std::set<std::thread::id> thread_ids;
 
-        std::this_thread::sleep_for(1s);
-        EXPECT_TRUE(v.size() == TASK_COUNT);
-        for (size_t i = 0; i < TASK_COUNT; ++i) {
-            EXPECT_EQ(i, v[i].get());
-        }
-        EXPECT_EQ(pool.queueSize(), 0u);
+    ThreadPool pool{THREAD_COUNT};
+    std::vector<std::future<void>> futures;
+    for (size_t i = 0; i < TASK_COUNT; ++i) {
+        futures.push_back(pool.submit([&] {
+            std::this_thread::sleep_for(1ms);
+            std::lock_guard<std::mutex> l{mutex};
+            ++result;
+            thread_ids.insert(std::this_thread::get_id());
+        }));
     }
+    for (auto& f : futures) {
+        f.wait();
+    }
+    EXPECT_EQ(pool.queueSize(), 0);
+    EXPECT_EQ(result, TASK_COUNT);
+    EXPECT_EQ(thread_ids.size(), THREAD_COUNT);
 }
-
-struct StringTask {
-    StringTask(std::set<std::thread::id>& s, std::mutex& m, size_t task_num)
-        : _ids{s}, _mutex{m}, _task_num{task_num} {}
-    std::string operator()() {
-        const auto id = std::this_thread::get_id();
-        {
-            std::unique_lock<std::mutex> lock{_mutex};
-            _ids.insert(id);
-        }
-        std::this_thread::sleep_for(1s);
-        return std::to_string(_task_num);
-    }
-    std::set<std::thread::id>& _ids;
-    std::mutex& _mutex;
-    size_t _task_num;
-};
 
 TEST(ThreadPool, VariousTypesOfTasks) {
-    std::mutex mutex;
-    std::set<std::thread::id> set;
-    const size_t TASK_COUNT{4u};
-    std::vector<std::future<typename std::result_of<IntegerTask()>::type>> vi;
-    std::vector<std::future<typename std::result_of<StringTask()>::type>> vs;
-    {
-        ThreadPool pool{4u};
-        for (size_t i = 0; i < TASK_COUNT; ++i) {
-            vi.push_back(pool.submit(IntegerTask(set, mutex, i)));
-        }
-        for (size_t i = 0; i < TASK_COUNT; ++i) {
-            vs.push_back(pool.submit(StringTask(set, mutex, i)));
-        }
-        std::this_thread::sleep_for(1s);
-        for (size_t i = 0; i < TASK_COUNT; ++i) {
-            EXPECT_EQ(i, vi[i].get());
-            EXPECT_EQ(std::to_string(i), vs[i].get());
-        }
-        EXPECT_EQ(pool.queueSize(), 0u);
-    }
+    ThreadPool pool{2u};
+    auto fi = pool.submit([] { return 42; });
+    auto fs = pool.submit([] { return std::string{"42"}; });
+    EXPECT_EQ(fi.get(), 42);
+    EXPECT_EQ(fs.get(), std::string{"42"});
 }
 
 TEST(ThreadPool, Lambdas) {
     const size_t TASK_COUNT{4u};
     std::vector<std::future<size_t>> v;
-    {
-        ThreadPool pool{4u};
-        for (size_t i = 0; i < TASK_COUNT; ++i) {
-            v.push_back(pool.submit([task_num = i] {
-                std::this_thread::sleep_for(1s);
-                return task_num;
-            }));
-        }
-        for (size_t i = 0; i < TASK_COUNT; ++i) {
-            EXPECT_EQ(i, v[i].get());
-        }
-        EXPECT_EQ(pool.queueSize(), 0u);
+    ThreadPool pool{4u};
+    for (size_t i = 0; i < TASK_COUNT; ++i) {
+        v.push_back(pool.submit([task_num = i] {
+            std::this_thread::sleep_for(1ms);
+            return task_num;
+        }));
     }
+    for (size_t i = 0; i < TASK_COUNT; ++i) {
+        EXPECT_EQ(i, v[i].get());
+    }
+    EXPECT_EQ(pool.queueSize(), 0u);
 }
 
 TEST(ThreadPool, Exception) {
@@ -166,6 +90,11 @@ TEST(ThreadPool, FunctionWithArgs) {
     EXPECT_EQ(f.get(), 4);
 }
 
+static std::thread::id test_function(size_t delay) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(delay + 1));
+    return std::this_thread::get_id();
+}
+
 TEST(ThreadPool, ThreadsAreReused) {
     const size_t THREAD_COUNT{4u};
     ThreadPool pool{THREAD_COUNT};
@@ -173,31 +102,26 @@ TEST(ThreadPool, ThreadsAreReused) {
     std::set<std::thread::id> thread_ids;
 
     for (size_t i = 0u; i < THREAD_COUNT; ++i) {
-        futures.push_back(pool.submit([delay = i] {
-            std::this_thread::sleep_for(std::chrono::milliseconds(delay + 1));
-            std::cout << std::this_thread::get_id() << std::endl;
-            return std::this_thread::get_id();
-        }));
+        futures.push_back(pool.submit(test_function, i));
     }
 
     for (size_t i = 0u; i < THREAD_COUNT; ++i) {
         auto r = futures[i].get();
-        std::cout << r << std::endl;
-        thread_ids.insert(r);
+        auto iter = thread_ids.insert(r);
+        // New thread is used
+        EXPECT_TRUE(iter.second);
     }
 
     futures.clear();
 
     for (size_t i = 0u; i < THREAD_COUNT; ++i) {
-        futures.push_back(pool.submit([delay = i] {
-            std::this_thread::sleep_for(std::chrono::milliseconds(delay + 1));
-            std::cout << std::this_thread::get_id() << std::endl;
-            return std::this_thread::get_id();
-        }));
+        futures.push_back(pool.submit(test_function, i));
     }
 
     for (size_t i = 0; i < THREAD_COUNT; ++i) {
         auto r = futures[i].get();
-        EXPECT_TRUE(thread_ids.find(r) != thread_ids.end());
+        auto iter = thread_ids.find(r);
+        EXPECT_TRUE(iter != thread_ids.end());
+        thread_ids.erase(iter);
     }
 }
